b9527a8179866e7ed72430af15dbc2b11559c8ce
Carlo Lucchesi <carlo.lucchesi@edu.unifi.it>
2025-11-28 16:10:39 +0100

    Introduction of increasing load tests using sources with padding.

--- inc_load.py
+++ inc_load.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+import sys
 
 def execute_test(source_dir, target, num, sec, out_dir, threads=1, iterations=6):
     if not os.path.exists(out_dir):
@@ -14,7 +14,8 @@ def main():
     release_dir = "./cmake-build-release/"
     padding_dir = "./cmake-build-padding/"
     out_dir = "results/"
-
+    padding = ("padding" in sys.argv)
+    
     sequential_name = "sequential"
     parallel_AOS = "parallel_AOS"
     parallel_AOS_simd = "parallel_AOS_simd"
@@ -23,8 +24,11 @@ def main():
     parallel_SOA_simd = "parallel_SOA_simd"
     parallel_SOA_simd_manual = "parallel_SOA_simd_manual"
 
-    source_list = [sequential_name, parallel_AOS, parallel_AOS_simd, parallel_AOS_simd_manual, parallel_SOA,
+    if not padding:
+        source_list = [sequential_name, parallel_AOS, parallel_AOS_simd, parallel_AOS_simd_manual, parallel_SOA,
                    parallel_SOA_simd, parallel_SOA_simd_manual]
+    else:
+        source_list = [sequential_name, parallel_AOS, parallel_AOS_simd, parallel_AOS_simd_manual]
 
     base_threads = 8
     base_seconds = 10
@@ -32,7 +36,10 @@ def main():
     # Generating test for increasing load
     for l in range(1000, 15001, 1000):
         for name in source_list:
-            execute_test(release_dir, name, l, base_seconds, out_dir + name + f"_load{l}", base_threads)
+            if not padding:
+                execute_test(release_dir, name, l, base_seconds, out_dir + name + f"_load{l}", base_threads)
+            else:
+                execute_test(padding_dir, name, l, base_seconds, out_dir + name + f"_padding_load{l}", base_threads)
 
 
 main()
